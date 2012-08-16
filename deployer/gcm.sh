c8f1e9bbd7f66d1c0932b3123b74a5378ef3fa64
sebastien.lorber <lorber.sebastien@gmail.com>
2012-08-16 18:49:45 +0200

    add files to m2 repository

--- deployer/gcm.sh
+++ deployer/gcm.sh
@@ -1,5 +1,4 @@
 
 
-# Actually the deployment with altDeploymentRepository doesn't work, you'll have to move the files youself if needed
 
-mvn install:install-file -DaltDeploymentRepository=../releases -Dfile=gcm-server.jar -Dsources=gcm-server-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-server -Dversion=1.0.2 -DpomFile=gcm.pom -Dpackaging=jar -DcreateChecksum=true
+mvn install:install-file -Dmaven.repo.local=../releases -Dfile=gcm-server.jar -Dsources=gcm-server-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-server -Dversion=1.0.2 -DpomFile=gcm.pom -Dpackaging=jar -DcreateChecksum=true
