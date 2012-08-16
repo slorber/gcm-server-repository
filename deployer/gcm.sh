01f4b9023b2c04112023a26776389ee0341a6240
sebastien.lorber <lorber.sebastien@gmail.com>
2012-08-16 18:18:30 +0200

    readme

--- deployer/gcm.sh
+++ deployer/gcm.sh
@@ -1,4 +1,5 @@
 
 
+# Actually the deployment with altDeploymentRepository doesn't work, you'll have to move the files youself if needed
 
 mvn install:install-file -DaltDeploymentRepository=../releases -Dfile=gcm-server.jar -Dsources=gcm-server-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-server -Dversion=1.0.2 -DpomFile=gcm.pom -Dpackaging=jar -DcreateChecksum=true
